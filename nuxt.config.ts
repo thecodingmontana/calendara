a130ec4ea1199678fc50826af4e9d88cf1f572bd
thecodingmontana <dev.humblechris254@gmail.com>
2025-01-27 13:50:25 +0300

    chore(app): add commitlint and husky

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
   modules: [
     '@nuxtjs/color-mode',
     '@nuxt/eslint',
     'shadcn-nuxt',
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
   ],
+  devtools: { enabled: true },
+  colorMode: {
+    preference: 'system', // default value of $colorMode.preference
+    fallback: 'light', // fallback value if not system preference found
+    hid: 'nuxt-color-mode-script',
+    globalName: '__NUXT_COLOR_MODE__',
+    componentName: 'ColorScheme',
+    classPrefix: '',
+    classSuffix: '',
+    storage: 'localStorage', // or 'sessionStorage' or 'cookie'
+    storageKey: 'calendara-color-mode',
+  },
   future: {
     compatibilityVersion: 4,
   },
+  compatibilityDate: '2024-11-01',
   eslint: {
     config: {
-      stylistic: true
-    }
+      stylistic: true,
+    },
   },
   shadcn: {
     /**
@@ -25,6 +36,6 @@ export default defineNuxtConfig({
      * Directory that the component lives in.
      * @default "./components/ui"
      */
-    componentDir: './components/ui'
-  }
+    componentDir: './components/ui',
+  },
 })
