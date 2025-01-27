19e7199b52be5b72ff0b95445d8994fed1250edb
thecodingmontana <dev.humblechris254@gmail.com>
2025-01-27 12:05:04 +0300

    feat(app): add shadcn UI support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/color-mode', '@nuxt/eslint'],
+  modules: [
+    '@nuxtjs/color-mode',
+    '@nuxt/eslint',
+    'shadcn-nuxt',
+    '@nuxtjs/tailwindcss'
+  ],
   future: {
     compatibilityVersion: 4,
   },
@@ -10,5 +15,16 @@ export default defineNuxtConfig({
     config: {
       stylistic: true
     }
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
   }
 })
