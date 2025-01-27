d61a8215294b3e4ea9a4700f661645d171e69f18
thecodingmontana <dev.humblechris254@gmail.com>
2025-01-27 11:54:07 +0300

    feat(app): setup linting and formatting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/color-mode', '@nuxt/eslint'],
+  future: {
+    compatibilityVersion: 4,
+  },
+  eslint: {
+    config: {
+      stylistic: true
+    }
+  }
 })
